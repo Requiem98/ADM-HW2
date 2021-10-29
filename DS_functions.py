bd35368fedbe19fad4a0d1d9451ee9f53e74f806
Requiem98 <amedeo.ranaldi@gmail.com>
2021-10-29 02:10:27 +0200

    aggiornato

--- DS_functions.py
+++ DS_functions.py
@@ -12,7 +12,11 @@ from scipy.stats import t
 from sklearn import linear_model as  lm
 import seaborn as sns
 
-## Generic functions ##
+"""============================================================================================================================"""
+
+"""Generic functions""" 
+
+"""============================================================================================================================"""
 
 def dateparse(time_as_a_unix_timestamp):
     return pd.to_datetime(time_as_a_unix_timestamp, unit='s')
@@ -38,7 +42,11 @@ def datetime_range24(startM=0, endM=0, delta=60):
     t = np.array(dts).reshape((len(dts)//2),2)
     return t
 
-## RQ1 functions ##
+"""============================================================================================================================"""
+
+"""RQ1 functions""" 
+
+"""============================================================================================================================"""
 
 #Compute the heatmap of null values for each of the columns of the dataset
 """da modificare"""
@@ -54,7 +62,13 @@ def statisticalIndex():
     box = steam.iloc[:,[8,9,10,11,16,17]].plot.box(subplots=True,figsize=(20,8));
     return data, box
 
-## RQ2 functions ##
+
+"""============================================================================================================================"""
+
+"""RQ2 functions""" 
+
+"""============================================================================================================================"""
+
 
 def numbersOfReviewsByApplication(n=0):
     #Create a slice of the dataframe with the column "app_name" and "review_id" and than group it by "app_name"
@@ -77,8 +91,11 @@ def numbersOfReviewsByApplication(n=0):
 
     #plot the result
 
-    plt.figure(figsize=(30,5))
-
+    plt.figure(figsize=(30,30))
+    plt.grid()
+    plt.xticks(rotation="vertical")
+    plt.xlabel("Number of reviews", labelpad=25.0, size="xx-large")
+    plt.ylabel("Videogames", labelpad=25.0, size="xx-large")
         
     plt.barh(val, height, color=my_cmap(my_norm(height)));
     
@@ -116,7 +133,11 @@ def raccomendedApp_purchase_free(h=0, t=0):
     
     return raccomendedApp
     
-## RQ3 functions ##
+"""============================================================================================================================"""
+
+"""RQ3 functions""" 
+
+"""============================================================================================================================"""
 
 def numbersOfReviewByTime(interv):
     #Setting the index timestamp_created in order to use between_time function
@@ -147,7 +168,172 @@ def numbersOfReviewByTime(interv):
     
     return dic
 
-## RQ6 functions ##
+"""============================================================================================================================"""
+
+"""RQ4 functions""" 
+
+"""============================================================================================================================"""
+
+# function that receives as parameters both the name of a data set and a list of languages’ 
+# names and returns a data frame filtered only with the reviews written in the provided languages.
+
+def filterByLang(data, languages):
+    
+    data.set_index("language", inplace=True)
+    
+    try:
+        out = data.loc[languages[0]]
+     
+        for lang in languages[1:]:
+            temp = data.loc[lang]
+            out = pd.concat([out, temp])
+    
+    finally:
+        data.reset_index(inplace=True)
+    
+    return out
+
+#function that return a dataframe containing the numbers of reviews grouped by languages sorted
+def reviewsByLanguage(n):
+    return steam[["language", "review_id"]].groupby(["language"], as_index=False).count().sort_values("review_id", ascending=False).head(n)
+
+#function that return a dataframe with the following columns:
+#   ||LANGUAGE||VOTES_FUNNY||TOT REVIEWS||PERCENTAGE||
+# 
+#  -Language: the language of the reviews (ordered by the most used)
+#  -Votes_funny: the count of the reviews that received at least one vote as funny
+#  -Tot reviews: the total count of reviews
+#  -percentage: the percentage of reviews that received at least one vote as funny
+
+def percentageOfFunny(n):
+    
+    #get the top n language and filter the dataset by those 
+    filteredByLang = filterByLang(steam, reviewsByLanguage(n)["language"].array)
+    
+    #get the revies with at least one vote as funny
+    warnings.filterwarnings("ignore")
+    ones = filteredByLang[["votes_funny"]]
+    ones.where(filteredByLang.votes_funny != 0, inplace=True)
+    ones = ones.dropna()
+    #Group it by language
+    ones = ones.groupby(["language"]).count()
+    #get the total number of reviews by language
+    tot = filteredByLang[["votes_funny"]].groupby(["language"]).count()
+    #Initialize the final dataset with the ones
+    final = ones
+    #Adding the numbers of reviews as column
+    final["tot reviews"] = tot
+    #Compute the percentage and add it to the dataset
+    final["percentage"] = final.swifter.apply(lambda row: row["votes_funny"]/row["tot reviews"], axis=1)
+    
+    return final
+
+#function that return a dataframe with the following columns:
+#   ||LANGUAGE||VOTES_HELPFUL||TOT REVIEWS||PERCENTAGE||
+# 
+#  -Language: the language of the reviews (ordered by the most used)
+#  -Votes_helpful: the count of the reviews that received at least one vote as helpful
+#  -Tot reviews: the total count of reviews
+#  -percentage: the percentage of reviews that received at least one vote as helpful
+
+def percentageOfHelpful(n):
+    
+    #get the top n language and filter the dataset by those 
+    filteredByLang = filterByLang(steam, reviewsByLanguage(n)["language"].array)
+    
+    #get the revies with at least one vote as helpful
+    warnings.filterwarnings("ignore")
+    ones = filteredByLang[["votes_helpful"]]
+    ones.where(filteredByLang.votes_funny != 0, inplace=True)
+    ones = ones.dropna()
+    #Group it by language
+    ones = ones.groupby(["language"]).count()
+    #get the total number of reviews by language
+    tot = filteredByLang[["votes_helpful"]].groupby(["language"]).count()
+    #Initialize the final dataset with the ones
+    final = ones
+    #Adding the numbers of reviews as column
+    final["tot reviews"] = tot
+    #Compute the percentage and add it to the dataset
+    final["percentage"] = final.swifter.apply(lambda row: row["votes_helpful"]/row["tot reviews"], axis=1)
+    
+    return final
+
+"""============================================================================================================================"""
+
+"""RQ5 functions""" 
+
+"""============================================================================================================================"""
+
+
+#Return a dataframe with the steam_id as index and the number of reviews of the author as column
+def mostPopularReviewers(n):
+    return steam[["author.steamid", "author.num_reviews"]].groupby(["author.steamid"]).first().sort_values("author.num_reviews",ascending=False).head(n)
+
+#Plot the result fo the "mostPopularReviews" function
+def mostPopularReviewers_plot(n):
+    
+    #get the data from the "mostPopularReviews" function
+    data = mostPopularReviewers(n)
+    
+    #get the values and the height of the values from the dataset
+    height = data["author.num_reviews"].array
+    val = list(map(str,data.index))
+
+    #map the colors with the height
+    my_cmap = plt.get_cmap('Greys')
+    my_norm = plt.Normalize(vmin=0,vmax=(data["author.num_reviews"].max())*0.2)
+    
+    plt.figure(figsize=(30,5))
+    
+    plot = plt.bar(val, height, color=my_cmap(my_norm(height)));
+    plt.xlabel('author.steamid', labelpad=25.0, size="xx-large")
+    plt.ylabel('Number of reviews', labelpad=25.0, size="xx-large")
+    plt.title('Reviews by author')
+    plt.grid()
+    if(n > 10):
+        plt.xticks(rotation="vertical")
+        
+    return plot
+
+#function that return a list of games that the most popular reviewer reviewed
+def reviewedApplicationFromTheMostPopularReviewer():
+    return steam[steam['author.steamid']==mostPopularReviewers(1).index[0]].app_name
+
+def percentageOfReceived():
+    
+    freeAndPurchase = steam[['author.steamid', "steam_purchase", "received_for_free"]].groupby('author.steamid', as_index=False).sum()
+    
+    freeAndPurchase = freeAndPurchase[freeAndPurchase['author.steamid']==mostPopularReviewers(1).index[0]]
+    
+    percPurchase = freeAndPurchase[["steam_purchase", "received_for_free"]].apply(lambda row: row["steam_purchase"]/(row["steam_purchase"] + row["received_for_free"]),axis=1)
+    
+    percFree = freeAndPurchase[["steam_purchase", "received_for_free"]].apply(lambda row: row["received_for_free"]/(row["steam_purchase"] + row["received_for_free"]),axis=1)
+    
+    final = freeAndPurchase
+    
+    final["percentage_of_purchase"] = percPurchase
+    final["percentage_of_free"] = percFree
+    
+    return final
+
+#function that return a dataframe with the following columns:
+#   ||RECOMMENDED||STEAM_PURCHASE||RECEIVED_FOR_FREE||
+# 
+#  -recommended: If the apps are recommended
+#  -steam_purchase: number of purchased app that are reccomanded or not
+#  -received_for_free: number of app received for free that are reccomanded or not 
+def recommendedApp_purchasedAndFree():
+    data = steam[['author.steamid', "recommended", "steam_purchase", "received_for_free"]]
+    data = data[data['author.steamid']==mostPopularReviewers(1).index[0]]
+    return data.groupby("recommended", as_index=False).sum(numeric_only=True).drop(["author.steamid"], axis=1)
+
+
+"""============================================================================================================================"""
+
+"""RQ6 functions""" 
+
+"""============================================================================================================================"""
 
 def timedelta_updated_created():
     return steam[["timestamp_created", "timestamp_updated"]].swifter.apply(lambda row:row["timestamp_updated"] - row["timestamp_created"], axis=1)
@@ -223,7 +409,11 @@ def numberOfUpdateAndNonUpdateByAuthor(n):
     #Sort and return the first n
 	return AuthorTOT.sort_values(["Update percentage", "TOT"], ascending=False).head(n)  
 
-## RQ7 functions ##
+"""============================================================================================================================"""
+
+"""RQ7 functions""" 
+
+"""============================================================================================================================"""
 
 def probabilityQuestion1():
 	#NUmber of total cases
@@ -263,7 +453,7 @@ def probabilityQuestion3():
 
 
 
-steam = pd.read_csv("./steam_reviews.csv", header="infer", parse_dates=['timestamp_created', 'timestamp_updated', 'author.last_played'], date_parser=dateparse, index_col=0,nrows=20)
+steam = pd.read_csv("./steam_reviews.csv", header="infer", parse_dates=['timestamp_created', 'timestamp_updated', 'author.last_played'], date_parser=dateparse, index_col=0)
 
 steam['author.playtime_last_two_weeks']=pd.to_timedelta(steam['author.playtime_last_two_weeks'], unit='m')
 steam['author.playtime_forever']=pd.to_timedelta(steam['author.playtime_forever'], unit='m')
